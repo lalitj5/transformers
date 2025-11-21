5502cba6ac818e2e54e46774d18ddfe8650fd7a4
Lalit J <lalitjulapalli5@gmail.com>
2025-11-21 14:43:50 -0500

    quick file reading

--- attention.py
+++ attention.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+
+# opening the text file
+file_path = "data/shakespeare.txt"
+
+input_file = open(file_path, "r")
+
+text_body = input_file.read()
+
+input_file.close() # must include this close otherwise there will be a exit error
+
+
+
