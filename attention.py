a8f2203ab5c0dabeec6b784dfd76062fca69dea9
Lalit J <lalitjulapalli5@gmail.com>
2025-11-22 02:01:07 -0500

    created characters to encode

--- attention.py
+++ attention.py
@@ -7,9 +7,16 @@ file_path = "data/shakespeare.txt"
 
 input_file = open(file_path, "r")
 
-text_body = input_file.read()
+text = input_file.read()
 
 input_file.close() # must include this close otherwise there will be a exit error
 
+# print(len(text)) # 1115394
+
+char_list = sorted(list(set(text))) # gets all possible characters seen in text
+print("".join(char_list)) # first character is a \n and second is a space
+
+# encoder (Assign each character to a number)
 
 
+# decoder
