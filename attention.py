f4ef094cc914eeee97b2bf67de2ce6a0da8545b1
Lalit J <lalitjulapalli5@gmail.com>
2025-11-22 12:08:48 -0500

    basic vector encoder and debugger

--- attention.py
+++ attention.py
@@ -14,9 +14,28 @@ input_file.close() # must include this close otherwise there will be a exit erro
 # print(len(text)) # 1115394
 
 char_list = sorted(list(set(text))) # gets all possible characters seen in text
-print("".join(char_list)) # first character is a \n and second is a space
+# print("".join(char_list)) # first character is a \n and second is a space
 
 # encoder (Assign each character to a number)
+# enum = dict(enumerate(char_list)) # dictionary of all characters mapped to number from 0-65
+# print(enum)
 
+def encode(tokens):
+    encoding = []
+    for char in tokens:
+        encoding.append(char_list.index(char))
+    
+    return encoding
+
+print(encode("point"))
 
 # decoder
+
+def decode(vector):
+    string = ""
+    for item in vector:
+        string = string + char_list[item]
+    
+    return string
+
+print(decode(encode("point")))
