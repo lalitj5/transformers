c8d5a5a7f9922de56e96958a9daeed5f3ecfc1f9
Lalit J <lalitjulapalli5@gmail.com>
2025-11-23 23:23:33 -0500

    fixed scaled dot product attention

--- attention.py
+++ attention.py
@@ -23,18 +23,16 @@ Output = Attention * V
 def scaled_dot_product_attention(Q, K, V, mask = None):
     # Q, K, V are tensors
     # d_k will be specified
+    d_k = K.shape[-1]
+    attention = ((Q @ K.transpose(-2,-1)) / (d_k ** 0.5)).softmax(-1)
 
-    d_k = 32
-    attention = ((Q @ K.t()) / d_k).softmax(1)
+    # replace the values with -inf as softmax -inf = 0
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, float('-inf'))
 
     return attention @ V
 
-Q = torch.rand(2,2)
-K = torch.rand(2,2)
-V = torch.rand(2,2)
-
-print(Q)
-print(K)
-print(V)
-
+Q = torch.rand(4, 8, 64)
+K = torch.rand(4, 8, 64)
+V = torch.rand(4, 8, 64)
 print(scaled_dot_product_attention(Q,K,V))
