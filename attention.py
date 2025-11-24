72881aa9026b320ce85b5b384678aecbb0490791
Lalit J <lalitjulapalli5@gmail.com>
2025-11-23 19:02:35 -0500

    scaled dot product attention

--- attention.py
+++ attention.py
@@ -2,12 +2,10 @@ import numpy as np
 import matplotlib as plot
 import torch
 
-from data import encode
-from data import decode
-from data import get_batch
+from data import encode, decode, get_batch
 
 import config
-
+"""
 xb, yb = get_batch("train")
 
 # how the transformer looks ahead
@@ -16,11 +14,27 @@ for b in range(config.batch_size):
         context = xb[b, :i+1] # 2d array accessing
         target = yb[b, i]
         # print(f"When we have {context} the next is {target}")
+"""
 
 """
 Attention = softmax(QK^T/sqrt(d_k))
 Output = Attention * V
 """
+def scaled_dot_product_attention(Q, K, V, mask = None):
+    # Q, K, V are tensors
+    # d_k will be specified
 
-dk = config.batch_size
-print(dk)
+    d_k = 32
+    attention = ((Q @ K.t()) / d_k).softmax(1)
+
+    return attention @ V
+
+Q = torch.rand(2,2)
+K = torch.rand(2,2)
+V = torch.rand(2,2)
+
+print(Q)
+print(K)
+print(V)
+
+print(scaled_dot_product_attention(Q,K,V))
